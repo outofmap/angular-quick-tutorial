20311b8878b688de2ec5e6ef05c8ec9cf711eb99
solar-song <solar.song@kakaocorp.com>
2016-10-26 18:22:28 +0900

    selected item and css class when selected

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,11 +26,22 @@ const HEROES: Hero[] = [
 	template: `
 		<h2>My Heroes</h2>
 		<ul class="heroes">
-		  <li *ngFor="let hero of heroes">
+		  <li *ngFor="let hero of heroes" 
+		    [class.selected] = " hero === selectedHero" 
+		    (click)="onSelect(hero)">
+		    
 		    <span class="badge">{{hero.id}}</span> {{hero.name}}
 			<!-- each hero goes here -->
 		  </li>
 		</ul>
+		<div  *ngIf="selectedHero">
+	        <h2>{{selectedHero.name}} details!</h2>
+		    <div><label>id: </label>{{selectedHero.id}}</div>
+		  <div>
+			<label>name: </label>
+			<input [(ngModel)]="selectedHero.name" placeholder="name"/>
+		  </div>
+		 </div>
 		`,
 	styles : [ `
 	  .selected {
@@ -85,10 +96,11 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero : Hero = {
-		id: 1,
-		name: 'Windstrom'
-	}
 	heroes = HEROES;
+	selectedHero : Hero;
+
+	onSelect(hero : Hero) : void {
+		this.selectedHero = hero;
+	}
 
 }
