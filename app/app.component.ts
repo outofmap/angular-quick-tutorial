cb80dd0e8b0e38effb77675360b1e5c9b4fe942c
solar-song <solar.song@kakaocorp.com>
2016-10-26 17:16:03 +0900

    add config files, module, component

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by solar on 2016. 10. 26..
+ */
+import { Component } from '@angular/core';
+@Component({
+	selector: 'my-app',
+	template: `<h1>My First Angular App</h1>`
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by solar on 2016. 10. 26..
+ */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+	imports:      [ BrowserModule ]
+})
+export class AppModule {
+
+}
