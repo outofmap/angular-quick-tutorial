eeea93802ada164a9750c4e34ec16dfcb11ac18b
solar-song <solar.song@kakaocorp.com>
2016-10-26 17:19:36 +0900

    add browsermodule in app module and main to startapp

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@
  */
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppComponent }   from './app.component';
 @NgModule({
-	imports:      [ BrowserModule ]
+	imports:      [ BrowserModule ],
+	declarations: [ AppComponent ],
+	bootstrap:    [ AppComponent ]
 })
-export class AppModule {
+export class AppModule { }
 
-}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by solar on 2016. 10. 26..
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
