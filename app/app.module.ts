1fc5d3830c03642a5dd6f55f7d9172e63fbc93c8
solar-song <solar.song@kakaocorp.com>
2016-10-31 11:50:53 +0900

    add httpModule in appModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,11 +11,13 @@ import {HeroesComponent} from "./heroes.component";
 import {HeroService} from "./hero.service";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
 	imports:      [
 		BrowserModule,
 		FormsModule,
+		HttpModule,
 		AppRoutingModule
 	],
 	declarations: [
